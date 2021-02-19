2ee55cc878982d80150e90e607db838669caa3e7
bharat.patel <bharat.patel@getcruise.com>
2021-02-19 11:11:09 -0800

    fix unwanted code

--- pkg/algo/overlap_interval_test.go
+++ pkg/algo/overlap_interval_test.go
@@ -9,13 +9,13 @@ import (
 
 func TestOverlappingIntervals(t *testing.T) {
 	input := []algo.Interval{
-		algo.Interval{Start: 1, End: 2, Group: "a"},
-		algo.Interval{Start: 3, End: 5, Group: "b"},
-		algo.Interval{Start: 2, End: 3, Group: "a"},
-		algo.Interval{Start: 6, End: 9, Group: "b"},
-		algo.Interval{Start: 8, End: 9, Group: "c"},
-		algo.Interval{Start: 7, End: 10, Group: "a"},
-		algo.Interval{Start: 12, End: 13, Group: "b"},
+		{Start: 1, End: 2, Group: "a"},
+		{Start: 3, End: 5, Group: "b"},
+		{Start: 2, End: 3, Group: "a"},
+		{Start: 6, End: 9, Group: "b"},
+		{Start: 8, End: 9, Group: "c"},
+		{Start: 7, End: 10, Group: "a"},
+		{Start: 12, End: 13, Group: "b"},
 	}
 
 	a, err := algo.FindOverlappingIntervals(input, []string{"a", "b", "c"})
